9b9ea98c27f06ed9a9cf628000ac8e6f9ff8bb3f
arturadamian <arturadamian@gmail.com>
2018-04-12 23:25:50 +0000

    typo

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,11 +2,11 @@
 if __name__ == "__main__":
     from sys import argv
     if len(argv) == 1:
-        print("0 arguments")
+        print("0 arguments:")
     elif len(argv) == 2:
-        print("1 argument")
+        print("1 argument:")
     else:
-        print("{} arguments".format(len(argv) - 1))
+        print("{} arguments:".format(len(argv) - 1))
         for i, s in enumerate(argv):
             if i != 0:
                 print("{}: {}".format(i, s))
