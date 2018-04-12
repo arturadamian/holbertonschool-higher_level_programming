30b163d2796c3382005ebf105da98fd6d4e5d509
arturadamian <arturadamian@gmail.com>
2018-04-12 23:22:29 +0000

    prints all arguments

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from sys import argv
+    if len(argv) == 1:
+        print("0 arguments")
+    elif len(argv) == 2:
+        print("1 argument")
+    else:
+        print("{} arguments".format(len(argv) - 1))
+        for i, s in enumerate(argv):
+            if i != 0:
+                print("{}: {}".format(i, s))
