a2762b2e9d66030ee706a59319a1a9ba94004937
arturadamian <arturadamian@gmail.com>
2018-04-18 03:25:08 +0000

    change to if else

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    if not sentence:
-        sentence[0] = None
+    if sentence:
+        letter = sentence[0]
+    else:
+        letter = None
     tuple = (len(sentence), sentence[0])
     return tuple
