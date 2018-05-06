84ac1a4b380ff75cc24ad36377a0cb1bbe86e658
arturadamian <arturadamian@gmail.com>
2018-05-06 06:35:42 +0000

    adds two ints

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -7,7 +7,14 @@ add_integer - adds two integers
 
 def add_integer(a, b=98):
     """
-    Args: a, b - parameters
+    Args:
+    a (int, float): first parameter
+    b (int, float): second parameter
+
+    Raises:
+
+    TypeError: a must be an integer
+    TypeError: a must be an integer
     """
     if not isinstance(a, (float, int)):
         raise TypeError("a must be an integer")
