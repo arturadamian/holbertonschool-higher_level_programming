cfcd603b6460ceffbccfb9f411c2b49178103f98
arturadamian <arturadamian@gmail.com>
2018-05-08 20:41:46 +0000

    change repr

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -46,7 +46,7 @@ class Rectangle:
         ValueError: width must be >= 0
 
         """
-        if not isinstance(value, (int, float)):
+        if not isinstance(value, int):
             raise TypeError("width must be an integer")
         if value < 0:
             raise ValueError("width must be >= 0")
@@ -64,7 +64,7 @@ class Rectangle:
         ValueError: height must be >= 0
 
         """
-        if not isinstance(value, (int, float)):
+        if not isinstance(value, int):
             raise TypeError("height must be an integer")
         if value < 0:
             raise ValueError("height must be >= 0")
