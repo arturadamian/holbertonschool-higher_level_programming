d344dba1735b642c7edce536df648bb18f21d2a8
arturadamian <arturadamian@gmail.com>
2018-05-17 06:17:37 +0000

    comments

--- 0x0B-python-input_output/9-add_item.py
+++ 0x0B-python-input_output/9-add_item.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+""" adds all arguments to a Python list"""
 save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
 
