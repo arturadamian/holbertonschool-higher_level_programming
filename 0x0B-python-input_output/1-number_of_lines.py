974aaec2afc6d5c45b68fecfee8ce87fd68a81b8
arturadamian <arturadamian@gmail.com>
2018-05-17 02:52:32 +0000

    counts lines

---
+++ 0x0B-python-input_output/1-number_of_lines.py
@@ -0,0 +1,14 @@
+#!usr/bin/python3
+
+
+def number_of_lines(filename=""):
+    """returns the number of lines
+
+    Args:
+        filename (str): filenme
+    """
+    count = 0
+    with open(filename, "r") as f:
+        for line in f:
+            count += 1
+    return count
