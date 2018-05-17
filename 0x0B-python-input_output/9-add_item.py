dfea7cd37f7dfe4344c9b961d4fc30389bbc5e8d
arturadamian <arturadamian@gmail.com>
2018-05-17 22:29:21 +0000

    module import t top of the file

--- 0x0B-python-input_output/9-add_item.py
+++ 0x0B-python-input_output/9-add_item.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 """ adds all arguments to a Python list"""
+from sys import argv
+
 save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
 
-from sys import argv
 listik = argv[1:]
 try:
     obj = load_from_json_file("add_item.json")
