b787f1435af46a5a5026988a2288b28eeb16c58c
Artur Adamian <arturadamian@gmail.com>
2018-05-22 19:43:58 -0700

    f***

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -16,10 +16,11 @@ class Base:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
 
+    @staticmethod
     def to_json_string(list_dictionaries):
         """that returns the JSON string representation of list_dictionaries"""
 
-        if not list_dictionaries or len(list_dictionaries) == 0:
+        if not list_dictionaries:
             return "[]"
         return json.dumps(list_dictionaries)
 
