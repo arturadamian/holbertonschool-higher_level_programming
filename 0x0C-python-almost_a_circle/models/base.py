a75cf5e76fbf5b135fd9412e2359b376860e493d
Artur Adamian <arturadamian@gmail.com>
2018-05-22 19:19:59 -0700

    not checking if its empty

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -18,7 +18,7 @@ class Base:
 
     def to_json_string(list_dictionaries):
         """that returns the JSON string representation of list_dictionaries"""
-        if not list_dictionaries and not any(list_dictionaries):
+        if list_dictionaries is None:
             return "[]"
         return json.dumps(list_dictionaries)
 
