cdf38b7d97871f746e9a77840e73c11ea50744c8
Artur Adamian <arturadamian@gmail.com>
2018-05-22 20:36:23 -0700

    tests base

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -1,38 +1,32 @@
 #!/usr/bin/python3
+"""testing Base class code"""
 import unittest
 from models.base import Base
 
+
 class TestBaseMethods(unittest.TestCase):
-    
+    """class with tests"""
+
     def check_id(self):
-        
+        """checking the id"""
+
         self.assertEqual(None, 1)
         self.assertEqual(Base(12), 12)
         self.assertEqual(None(), 2)
         self.assertEqual(Base(-1), -1)
-        
+
     def to_js_str(self):
+        """checking the dumps method"""
 
         self.assertEqual(None, "[]")
         result = Base.to_json_string([dictionary])
-        self.assertTrue(type(result), <class 'str'>)
-        self.assertTrue(type(list_dictionaries), <class 'dict'>)
+#        self.assertTrue(type(result), <class 'str'>)
+#        self.assertTrue(type(list_dictionaries), <class 'dict'>)
 
+    def test_doc(self):
+        """testing docstring"""
 
+        self.assertIsNotNone(Base.__doc__)
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-    
 if __name__ == '__main__':
     unittest.main()
