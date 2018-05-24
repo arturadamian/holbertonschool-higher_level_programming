5a8ddaef0b639ef22ea56cc0c41b34173b3bd195
Artur Adamian <arturadamian@gmail.com>
2018-05-24 15:32:55 -0700

    first test

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 """testing Base class code"""
+import os
+import sys
+import json
 import unittest
 from models.base import Base
 
@@ -7,19 +10,23 @@ from models.base import Base
 class TestBaseMethods(unittest.TestCase):
     """class with tests"""
 
-    def check_id(self):
+    def test_check_id(self):
         """checking the id"""
+        r1 = Base()
+        self.assertEqual(r1.id, 1)
+        r2 = Base(12)
+        self.assertEqual(r2.id, 12)
+        r3 = Base()
+        self.assertEqual(r3.id, 2)
+        r4 = Base(-1)
+        self.assertEqual(r4.id, -1)
 
-        self.assertEqual(None, 1)
-        self.assertEqual(Base(12), 12)
-        self.assertEqual(None(), 2)
-        self.assertEqual(Base(-1), -1)
-
-    def to_js_str(self):
+    def test_to_js_str(self):
         """checking the dumps method"""
 
-        self.assertEqual(None, "[]")
-        result = Base.to_json_string([dictionary])
+        result = Base.to_json_string([])
+        self.assertEqual(result, "[]")
+#        result = Base.to_json_string([dictionary])
 #        self.assertTrue(type(result), <class 'str'>)
 #        self.assertTrue(type(list_dictionaries), <class 'dict'>)
 
