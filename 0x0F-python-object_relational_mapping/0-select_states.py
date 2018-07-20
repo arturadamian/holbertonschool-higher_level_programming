fb5d45e68714f6cabf5c9efd7af73fc75de15038
arturadamian <arturadamian@gmail.com>
2018-07-20 23:32:06 +0000

    tab indentation

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -12,10 +12,10 @@ if __name__ == "__main__":
         user=sys.argv[1],
         passwd=sys.argv[2],
         db=sys.argv[3])
-cur = db.cursor()
-cur.execute("SELECT * FROM states ORDER BY id")
-rows = cur.fetchall()
-for row in rows:
-    print(row)
-cur.close()
-db.close()
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states ORDER BY id")
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+    cur.close()
+    db.close()
