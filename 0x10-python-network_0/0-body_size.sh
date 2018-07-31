b9c453d3b2fbdce362cbd5ee9673e74b18efcd7a
arturadamian <arturadamian@gmail.com>
2018-07-31 23:30:57 +0000

    single quotes for awk

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # takes in a URL, sends a request to that URL, and displays the size
-curl -sI "$1" |grep -i Content-length | awk "{print "$2"}"
+curl -sI "$1" |grep -i Content-length | awk '{print "$2"}'
