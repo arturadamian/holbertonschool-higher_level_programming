065fa06333ef7039c5097e83f1fad857427c7edc
arturadamian <arturadamian@gmail.com>
2018-08-06 20:49:36 +0000

    fetches the data with urllib.request

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+# fetches URL with urllib
+import urllib.request
+
+if __name__ == "__main__":
+
+    with urllib.request.urlopen('https://intranet.hbtn.io/status') as responce:
+        html = responce.read()
+        code = html.decode('utf8')
+        print("Body response:")
+        print("\t- type:{}".format(type(html)))
+        print("\t- cotent:{}".format(html))
+        print("\t- utf8 content:{}".format(code))
