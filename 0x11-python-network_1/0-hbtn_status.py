26893ae33402b35284eefaf94ce832965f88e83c
arturadamian <arturadamian@gmail.com>
2018-08-07 03:32:41 +0000

    coNtent wrong

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -13,5 +13,5 @@ if __name__ == "__main__":
 
     print("Body response:")
     print("\t- type: {}".format(type(html)))
-    print("\t- cotent: {}".format(html))
+    print("\t- content: {}".format(html))
     print("\t- utf8 content: {}".format(html.decode('utf-8')))
