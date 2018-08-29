26684114705776b2b37b9a7337ea49c15c2c5270
arturadamian <arturadamian@gmail.com>
2018-08-29 04:27:41 +0000

    add numbers(args)

---
+++ 0x12-javascript-warm_up/9-add.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+let a = parseInt(process.argv[2]);
+let b = parseInt(process.argv[3]);
+function add (x, y) {
+  return x + y;
+}
+if (isNaN(a) || isNaN(b)) {
+  console.log('NAN');
+} else {
+  console.log(add(a, b));
+}
