d0042daa93dc64510ae8ee250a2dcf3256368073
arturadamian <arturadamian@gmail.com>
2018-08-29 04:28:43 +0000

    tab's

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -3,10 +3,10 @@
 let a = parseInt(process.argv[2]);
 let b = parseInt(process.argv[3]);
 function add (x, y) {
-  return x + y;
+    return x + y;
 }
 if (isNaN(a) || isNaN(b)) {
-  console.log('NAN');
+    console.log('NAN');
 } else {
-  console.log(add(a, b));
+    console.log(add(a, b));
 }
