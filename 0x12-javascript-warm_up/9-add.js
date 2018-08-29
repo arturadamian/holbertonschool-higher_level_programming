28fdb8fc89978deb663536d3d2888573828a8cb3
arturadamian <arturadamian@gmail.com>
2018-08-29 04:49:30 +0000

    NaN returned by default

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,14 +1,10 @@
 #!/usr/bin/node
 
-function add(x, y) {
-    return x + y;
+function add (x, y) {
+  return x + y;
 }
 
 let a = parseInt(process.argv[2]);
 let b = parseInt(process.argv[3]);
 
-if (isNaN(a) || isNaN(b)) {
-    console.log('NAN');
-} else {
-    console.log(add(a, b));
-}
+console.log(add(a, b));
